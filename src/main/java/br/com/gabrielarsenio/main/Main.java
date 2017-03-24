4c7aea1203ae7eb03bdd9d6432b5861e0850a18e
Gabriel Arsênio <gabrielarsenioo@gmail.com>
2017-03-24 20:43:21 -0300

    Initial commit

---
+++ src/main/java/br/com/gabrielarsenio/main/Main.java
@@ -0,0 +1,10 @@
+package br.com.gabrielarsenio.main;
+
+/**
+ * Gabriel Arsenio 24/03/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testeeeeeeeee");
+    }
+}
